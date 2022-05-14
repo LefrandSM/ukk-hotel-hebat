1d9020cd28780ffd6f5cb34e10221f95bbf5cb96
LefrandSM <lefrandseptiandm@gmail.com>
2022-05-14 20:37:08 +0700

    redirect user memaksa masuk ke route yang di batas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,39 +24,37 @@ class AuthController extends Controller
             'password' => 'required|max:20'
         ]);
 
-        if(Auth::guard('admin')->attempt(['username' => $credentials['username'], 'password' => $credentials['password']])) {
+        if (Auth::guard('admin')->attempt(['username' => $credentials['username'], 'password' => $credentials['password']])) {
             $request->session()->regenerate();
 
- 
             return redirect()->intended('/admin');
         }
 
-        if(Auth::guard('resepsionis')->attempt(['username' => $credentials['username'], 'password' => $credentials['password']])) {
+        if (Auth::guard('resepsionis')->attempt(['username' => $credentials['username'], 'password' => $credentials['password']])) {
             $request->session()->regenerate();
 
- 
             return redirect()->intended('/resepsionis');
         }
         return redirect('/login')->with('gagal', 'Username atau password salah');
     }
     public function logout(Request $request)
     {
-        if(Auth::guard('admin')) {
+        if (Auth::guard('admin')) {
             Auth::guard('admin')->logout();
-        
+
             $request->session()->invalidate();
-        
+
             $request->session()->regenerateToken();
-        
+
             return redirect('/login');
         }
-        if(Auth::guard('resepsionis')) {
+        if (Auth::guard('resepsionis')) {
             Auth::guard('resepsionis')->logout();
-        
+
             $request->session()->invalidate();
-        
+
             $request->session()->regenerateToken();
-        
+
             return redirect('/login');
         }
     }
